87335b356d81eaf14bdcb8b2a81bc97d5dae5799
Andreas Gade Jensen <andreasgadej@gmail.com>
2019-02-11 20:13:19 +0100

    init configs

--- src/config/laravel-nova-config.php
+++ src/config/laravel-nova-config.php
@@ -1,5 +1,12 @@
 <?php 
 
 return [
-    
+    'app' => [
+        [
+            'label' => 'App name',
+            'name' => 'name',
+            'type' => 'text',
+            'value' => env('APP_NAME', 'Laravel nova')
+        ],
+    ],
 ];
